6af9c24ea3068eea52041b2007160902434f3db9
Paul <zhpv96@gmail.com>
2024-10-10 13:13:39 +0300

    fix depositFor

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,5 @@
 import { run } from '@subsquid/batch-processor'
 import { TypeormDatabase } from '@subsquid/typeorm-store'
-import {
-    TradeOrderEventOutput,
-    OpenOrderEventOutput,
-    CancelOrderEventOutput,
-    DepositEventOutput,
-    DepositForEventOutput,
-    WithdrawEventOutput,
-    WithdrawToMarketEventOutput,
-    Market,
-} from './abi/Market'
-import isEvent from './isEvent'
 import { dataSource, processBlocks } from './dataSource'
 import { handleOpenOrderEvent } from './openOrderEventHandler'
 import { handleTradeOrderEvent } from './tradeOrderEventHandler'
@@ -19,6 +8,7 @@ import { handleDepositEvent } from './depositEventHandler'
 import { handleDepositForEvent } from './depositForEventHandler'
 import { handleWithdrawEvent } from './withdrawEventHandler'
 import { handleWithdrawToMarketEvent } from './withdrawToMarketEventHandler'
+import { getEventType } from './types'
 
 const database = new TypeormDatabase()
 
@@ -41,20 +31,21 @@ run(dataSource, database, async (ctx) => {
     for (let idx = 0; idx < logs.length; idx++) {
         let log = logs[idx];
         let receipt = receipts[idx];
+        let event = getEventType(receipt.val1)
 
-        if (isEvent<OpenOrderEventOutput>('OpenOrderEvent', log, Market.abi)) {
+        if (event == 'OpenOrderEvent') {
             await handleOpenOrderEvent(log, receipt, openOrderEvents, orders, activeBuyOrders, activeSellOrders, balances, ctx);
-        } else if (isEvent<TradeOrderEventOutput>('TradeOrderEvent', log, Market.abi)) {
+        } else if (event == 'TradeOrderEvent') {
             await handleTradeOrderEvent(log, receipt, tradeOrderEvents, orders, activeBuyOrders, activeSellOrders, balances, ctx);
-        } else if (isEvent<CancelOrderEventOutput>('CancelOrderEvent', log, Market.abi)) {
+        } else if (event == 'CancelOrderEvent') {
             await handleCancelOrderEvent(log, receipt, cancelOrderEvents, orders, activeBuyOrders, activeSellOrders, balances, ctx);
-        } else if (isEvent<DepositEventOutput>('DepositEvent', log, Market.abi)) {
+        } else if (event == 'DepositEvent') {
             await handleDepositEvent(log, receipt, depositEvents, balances, ctx);
-        } else if (isEvent<DepositForEventOutput>('DepositForEvent', log, Market.abi)) {
+        } else if (event == 'DepositForEvent') {
             await handleDepositForEvent(log, receipt, depositForEvents, balances, ctx);
-        } else if (isEvent<WithdrawEventOutput>('WithdrawEvent', log, Market.abi)) {
+        } else if (event == 'WithdrawEvent') {
             await handleWithdrawEvent(log, receipt, withdrawEvents, balances, ctx);
-        } else if (isEvent<WithdrawToMarketEventOutput>('WithdrawToMarketEvent', log, Market.abi)) {
+        } else if (event == 'WithdrawToMarketEvent') {
             await handleWithdrawToMarketEvent(log, receipt, withdrawToMarketEvents, balances, ctx);
         }
     }
